a3434c9d7c92b5af8790c05201dc176c01260337
Diego <diego.aguileram@sansano.usm.cl>
2023-04-24 18:43:11 -0400

    fix:camelcamelcase

--- models/Categoria.js
+++ models/Categoria.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CategorySchema = new Schema({
+const categorySchema = new Schema({
     nombre : String,
     usuario: String,
     color : String 
 })
 
 
-CategorySchema.set('toJSON', {
+categorySchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
       delete returnedObject._id
@@ -16,4 +16,4 @@ CategorySchema.set('toJSON', {
     }
   })
   
-  module.exports = mongoose.model('Categoria', CategorySchema)
+  module.exports = mongoose.model('Categoria', categorySchema)
