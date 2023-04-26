98cf2cf623a7fa97e2b497159bb51c0e759b6645
PPYanez <pedro.yanez@sansano.usm.cl>
2023-04-25 21:40:47 -0400

    feat: create requestLogger middleware and use it on app

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
 const cors = require('cors')
+
 const errorHandler = require('./middleware/errorHandler')
-const mongoose = require('mongoose')
 const unknownEndpoint = require('./middleware/unknownEndpoint')
+const requestLogger = require('./middleware/requestLogger')
+
+const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -17,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
 // Endpoints
 // Para crear un endpoint, crear el handler en la carpeta controllers
