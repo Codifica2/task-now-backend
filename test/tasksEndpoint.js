6c074d8c89c7e418de9a8afb81dd25dd6690c827
PPYanez <pedro.yanez@sansano.usm.cl>
2023-05-11 23:37:07 -0400

    test: add register test and format other tests

--- test/tasksEndpoint.js
+++ test/tasksEndpoint.js
@@ -6,6 +6,7 @@ const Task = require('../models/Task');
 const Category = require('../models/Category');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+const User = require("../models/User");
 
 // Generate a fake verification token
 const generateFakeToken = (payload) => {
@@ -339,120 +340,141 @@ describe('Category Endpoint Test Case', () => {
         done()
       });
   })
+})
 
-  describe('Unit test for task endpoints', () => {
-    let token;
-    beforeEach(async () => {
-      try {const user = new User({ name: 'Sebita', lastName: 'Sebita', email: "sebita@usm.cl", password: "123456" });
-      await user.save();
-  
-      token = jwt.sign(
-        {
-          name: user.name,
-          id: user.id,
-        },
-        process.env.TOKEN_SECRET,
-      );
-      }catch (error) {
-      console.error('Error en beforeEach:', error);
-    }
-    });
-  
-  
-  
-    it('GET /api/tasks returns all existing tasks', async () => {
-  
-      const task1 = new Task({ title: 'Tarea 1', description: 'Descripción 1', due_date: '2025-05-05', category: 'category', status: 'status'});
-      const task2 = new Task({ title: 'Tarea 2', description: 'Descripción 2', due_date: '2025-05-06', category: 'category2', status: 'status2'});
-  
-      Promise.all([task1.save(), task2.save()]) // Promise.all para esperar a que se guarden ambas tareas
-        .then(() => {
-          return request(app)
-            .get('/api/tasks')
-            .set('Authorization', `Bearer ${token}`)
-            .expect(200)
-            .expect((res) => {
-              assert.strictEqual(res.body.status, 200)
-              assert.strictEqual(res.body.length, 2);
-            });
-        })
-        .then(() => {
-          done();
-        })
-        .catch((error) => {
-          done(error);
-        });
-    }).timeout(5000); // Aumentar el timeout a 5 segundos para que alcance a guardar las tareas
-  
-    it('POST /api/tasks create a new task', async () => {
-      const newTask = {
-        title: 'Nueva tarea',
-        description: 'Descripción de la nueva tarea',
-      };
-  
-      const response = await request(app)
-        .post('/api/tasks')
-        .set('Authorization', `Bearer ${token}`)
-        .send(newTask)
-        .set('Accept', 'application/json');
-  
-      assert.strictEqual(response.status, 201);
-      assert.strictEqual(response.body.title, newTask.title);
-      assert.strictEqual(response.body.description, newTask.description);
-  
-      const createdTask = await Task.findById(response.body.id);
-      assert.strictEqual(createdTask.title, newTask.title);
-      assert.strictEqual(createdTask.description, newTask.description);
-    });
-  
-    it('GET /api/tasks/:id returns a task', async () => {
-      const task = new Task({ title: 'Tarea', description: 'Descripción' });
-      await task.save();
-  
-      const response = await request(app).get(`/api/tasks/${task._id}`).set('Authorization', `Bearer ${token}`);
-      assert.strictEqual(response.status, 200);
-      assert.strictEqual(response.body.title, task.title);
-      assert.strictEqual(response.body.description, task.description);
-    });
-  
-    it('DELETE /api/tasks/:id delete a task', async () => {
-      const task = new Task({ title: 'Tarea', description: 'Descripción' });
-      await task.save();
-  
-      const response = await request(app).delete(`/api/tasks/${task._id}`).set('Authorization', `Bearer ${token}`);
-      assert.strictEqual(response.status, 200);
-  
-      const deletedTask = await Task.findById(task._id);
-      assert.strictEqual(deletedTask, null);
-    });
-  
-    it('PUT /api/tasks/:id update a task', async () => {
-      const task = new Task({ title: 'Tarea', description: 'Descripción' });
-      await task.save();
-  
-      const updatedTask = {
-        title: 'Tarea actualizada',
-        description: 'Descripción actualizada',
-      };
-  
-      const response = await request(app)
-        .put(`/api/tasks/${task._id}`)
-        .set('Authorization', `Bearer ${token}`)
-        .send(updatedTask)
-        .set('Accept', 'application/json');
-  
-      assert.strictEqual(response.status, 200);
-      assert.strictEqual(response.body.title, updatedTask.title);
-      assert.strictEqual(response.body.description, updatedTask.description);
-  
-      const fetchedTask = await Task.findById(task._id);
-      assert.strictEqual(fetchedTask.title, updatedTask.title);
-      assert.strictEqual(fetchedTask.description, updatedTask.description);
-    });
-  
-    afterEach(async () => {
-      await Task.deleteMany({});
-      await User.deleteMany({});
-    });
-  })
+describe("#register", async () => {
+  it("should create a new user in the database", async () => {
+    const user = {
+      name: "Pedro",
+      lastname: "Yáñez",
+      password: "123456",
+      email: "pedro.yanez@sansano.usm.cl",
+    };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(user)
+      .set("Accept", "application/json");
+
+    assert.strictEqual(response.status, 201);
+    assert.strictEqual(response.body.name, user.name);
+    assert.strictEqual(response.body.lastname, user.lastname);
+    assert.strictEqual(response.body.email, user.email);
+  });
 });
+
+describe('Unit test for task endpoints', () => {
+  let token;
+  beforeEach(async () => {
+    try {const user = new User({ name: 'Sebita', lastName: 'Sebita', email: "sebita@usm.cl", password: "123456" });
+    await user.save();
+
+    token = jwt.sign(
+      {
+        name: user.name,
+        id: user.id,
+      },
+      process.env.TOKEN_SECRET,
+    );
+    }catch (error) {
+    console.error('Error en beforeEach:', error);
+  }
+  });
+
+
+
+  it('GET /api/tasks returns all existing tasks', async () => {
+
+    const task1 = new Task({ title: 'Tarea 1', description: 'Descripción 1', due_date: '2025-05-05', category: 'category', status: 'status'});
+    const task2 = new Task({ title: 'Tarea 2', description: 'Descripción 2', due_date: '2025-05-06', category: 'category2', status: 'status2'});
+
+    Promise.all([task1.save(), task2.save()]) // Promise.all para esperar a que se guarden ambas tareas
+      .then(() => {
+        return request(app)
+          .get('/api/tasks')
+          .set('Authorization', `Bearer ${token}`)
+          .expect(200)
+          .expect((res) => {
+            assert.strictEqual(res.body.status, 200)
+            assert.strictEqual(res.body.length, 2);
+          });
+      })
+      .then(() => {
+        done();
+      })
+      .catch((error) => {
+        done(error);
+      });
+  }).timeout(5000); // Aumentar el timeout a 5 segundos para que alcance a guardar las tareas
+
+  it('POST /api/tasks create a new task', async () => {
+    const newTask = {
+      title: 'Nueva tarea',
+      description: 'Descripción de la nueva tarea',
+    };
+
+    const response = await request(app)
+      .post('/api/tasks')
+      .set('Authorization', `Bearer ${token}`)
+      .send(newTask)
+      .set('Accept', 'application/json');
+
+    assert.strictEqual(response.status, 201);
+    assert.strictEqual(response.body.title, newTask.title);
+    assert.strictEqual(response.body.description, newTask.description);
+
+    const createdTask = await Task.findById(response.body.id);
+    assert.strictEqual(createdTask.title, newTask.title);
+    assert.strictEqual(createdTask.description, newTask.description);
+  });
+
+  it('GET /api/tasks/:id returns a task', async () => {
+    const task = new Task({ title: 'Tarea', description: 'Descripción' });
+    await task.save();
+
+    const response = await request(app).get(`/api/tasks/${task._id}`).set('Authorization', `Bearer ${token}`);
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.body.title, task.title);
+    assert.strictEqual(response.body.description, task.description);
+  });
+
+  it('DELETE /api/tasks/:id delete a task', async () => {
+    const task = new Task({ title: 'Tarea', description: 'Descripción' });
+    await task.save();
+
+    const response = await request(app).delete(`/api/tasks/${task._id}`).set('Authorization', `Bearer ${token}`);
+    assert.strictEqual(response.status, 200);
+
+    const deletedTask = await Task.findById(task._id);
+    assert.strictEqual(deletedTask, null);
+  });
+
+  it('PUT /api/tasks/:id update a task', async () => {
+    const task = new Task({ title: 'Tarea', description: 'Descripción' });
+    await task.save();
+
+    const updatedTask = {
+      title: 'Tarea actualizada',
+      description: 'Descripción actualizada',
+    };
+
+    const response = await request(app)
+      .put(`/api/tasks/${task._id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send(updatedTask)
+      .set('Accept', 'application/json');
+
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.body.title, updatedTask.title);
+    assert.strictEqual(response.body.description, updatedTask.description);
+
+    const fetchedTask = await Task.findById(task._id);
+    assert.strictEqual(fetchedTask.title, updatedTask.title);
+    assert.strictEqual(fetchedTask.description, updatedTask.description);
+  });
+
+  afterEach(async () => {
+    await Task.deleteMany({});
+    await User.deleteMany({});
+  });
+})
